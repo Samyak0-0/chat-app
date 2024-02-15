2e360520cb031cd5aac9bec905f0ae41518cd465
samyak <samyakmhrzn9841@gmail.com>
2024-02-15 21:36:38 +0545

    getting beck to ths project

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -49,7 +49,7 @@ const Chat = (props: Props) => {
                     {Object.keys(onlinePeopleExcludingUser).map(userId => (
 
                         <div onClick={() => selectContact(userId)} className={" border-y-2 border-neutral-600 flex items-center cursor-pointer relative" + ((userId === selectedContact) ? " bg-neutral-600" : "")} key={userId}>
-                            <div className="h-full bg-sky-200 w-[2px] ml-1"></div>
+                            <div className="h-full bg-slate-200 w-[2px] ml-1"></div>
                             <div className="p-2 flex items-center">
                             <div className=" relative -top-1" ><Svgfunc seed={onlinePeople[userId]} sizze={0.09 * window.innerHeight} /></div>
                             <div className="ml-4 text-xl">{onlinePeople[userId]}</div>
@@ -59,13 +59,13 @@ const Chat = (props: Props) => {
                 </div>
             </div>
             <div className=" bg-slate-200 w-2/3 flex flex-col justify-between">
-                <div>
+                <div className="flex-grow">
                     {!selectedContact && (
-                        <div className="h-full w-full"> Select a Conversation to get Started </div>
+                        <div className="h-full w-full flex justify-center items-center text-neutral-400">&larr; Select a Conversation to get Started !</div>
                     )}
                 </div>
                 <div className="flex gap-1 mx-2 my-3">
-                    <input type="text" name="" id="" className=" w-full  rounded-md " />
+                    <input type="text" name="" id="" placeholder="Type your message here . . ." className=" w-full px-3 rounded-md " />
                     <button className=" p-2 text-white">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
